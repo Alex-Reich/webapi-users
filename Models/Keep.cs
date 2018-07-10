570f77a7b97734fd8c6d1670015a087a6d9c8ec4
Alex Reich <atrain.reich@gmail.com>
2018-07-10 14:07:16 -0600

    Set up keep repository

--- Models/Keep.cs
+++ Models/Keep.cs
@@ -8,9 +8,9 @@ namespace API_Users.Models
         public string Name {get; set;}
         public string Description {get; set;}
         public string userId {get; set;}
-        public bool Pub {get; set;} 
+        public bool Public {get; set;} 
         public int Views {get; set;}
         public int Shares {get; set;}
-        public int Keeps {get; set;}
+        public int Saves {get; set;}
     }
 }
---
+++ wwwsrc/src/store/index.js
@@ -0,0 +1,46 @@
+import vue from 'vue'
+import vuex from 'vuex'
+import axios from 'axios'
+import router from '../router'
+
+
+var production = !window.location.host.includes('localhost');
+var baseUrl = production ? '' : '//localhost:3000/';
+
+
+vue.use(vuex)
+
+var api = axios.create({
+    baseURL: baseUrl,
+    timeout: 3000,
+    withCredentials: true
+  })
+  var auth = axios.create({
+    baseURL: baseUrl + 'auth/',
+    timeout: 3000,
+    withCredentials: true
+  })
+
+export default new vuex.Store({
+    state: {
+        user: {}
+    },
+    mutations: {
+        setUser(state, user){
+            state.user = user
+        },
+        deleteUser(state){
+            state.user = {}
+        }
+    },
+    actions: {
+        login({commit}, loginCredentials){
+            auth.post('login', loginCredentials)
+            .then(res => {
+                console.log('Successfully logged in')
+                console.log(res.data)
+                commit('setUser', res.data.data)
+            })
+        }
+    }
+})  
