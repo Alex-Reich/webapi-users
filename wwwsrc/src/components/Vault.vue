4243ecd9c48996c66936bd06ddbabb82e74b6d7f
Alex Reich <atrain.reich@gmail.com>
2018-07-13 16:59:16 -0600

    Created vault component template

---
+++ wwwsrc/src/components/Vault.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+<h1>This is where I will draw my vaults</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    methods: {}
+  }
+
+</script>
+
+<style>
+
+
+</style>
